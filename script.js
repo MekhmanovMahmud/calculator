352b3367017b23b1f7ebb9eed11ede8ea55000b5
Mahmud <mahmudjon@gmail.com>
2021-11-14 18:00:11 +0500

    add project

---
+++ script.js
@@ -0,0 +1,18 @@
+var ekran = document.querySelector('.ekran')
+
+document.querySelectorAll('td').forEach(function (harbiriuchun) {
+   harbiriuchun.addEventListener('click', function (e) {
+      var belgi = e.target.innerText
+
+      if (belgi == "C") {
+         ekran.value = ""
+
+      } else if (belgi == "=") {
+         ekran.value = eval(ekran.value)
+
+      } else {
+         ekran.value += belgi
+      }
+   })
+})
+
